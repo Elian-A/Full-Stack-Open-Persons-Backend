0df031c7245b5797e107348139dc4e3953982095
Elian Avalos <elianemanuelstudy@gmail.com>
2022-03-15 14:55:24 -0300

    Step 4 Done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -46,6 +46,16 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  if (!persons.filter((person) => person.id === id)) res.status(404).end();
+
+  persons = persons.filter((person) => person.id !== id);
+  console.log(persons);
+  res.status(204).end();
 });
 
 const PORT = 3001;
