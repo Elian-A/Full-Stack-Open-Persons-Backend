8b42b50ddd219d2aabe1765d6824acac00357a33
Elian Avalos <elianemanuelstudy@gmail.com>
2022-03-16 14:28:18 -0300

    Added .gitignore prepared the app for heroku

--- index.js
+++ index.js
@@ -96,5 +96,5 @@ app.post("/api/persons", morgan(":object"), (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`API Running on Port ${PORT}`));
